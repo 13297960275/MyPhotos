a1ba8d3ce2895264ce17ad63767bc4cc296ebe8c
WH1607063 <WH1607063@PC-WH1607063>
2016-11-02 17:24:59 +0800

    测试MVC中ajax的用法。 工作需要，暂时搁置此项目，下面现总结一下项目进度：Users、Roles部分基本完成，后期需加入角色过滤；Comments部分未开始；Photos、Phototypes部分已完成主体的增删改查，尚有图片缩略图、水印、防盗链，图片下载、更改下载量、点击率、顶、踩功能未实现，且页面有待进一步优化，PagerIndex页面存在较大问题：分页后页面选择栏未固定，图片未使用缩略图、水印、防盗链以及图片下载、更改下载量、点击率、顶、踩等响应按钮事件功能未实现，考虑使用bootstrap弹出框结合ajax实现前后台交互。 整个项目可以做成一个图片资源网站，也可以根据用户实现个人相册管理集，当然，只是后话。最终也将做成web app。

---
+++ MvcAjax/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using MvcAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAjax.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Person(int? id)
+        {
+            //could add code here to get model based on id....
+            return PartialView("_Person");
+            //calling partial with existing model....
+            //return PartialView("_Person", model);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Person(PersonValidationViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = GetErrorsFromModelState();
+                return Json(new { success = false, errors = errors });
+                //return PartialView("_Person", model);
+            }
+
+            //save to persistent store;
+            //return data back to post OR do a normal MVC Redirect....
+            return Json(new { success = true });  //perhaps you want to do more on return.... otherwise this if block is not necessary....
+            //return RedirectToAction("Index");
+        }
+
+        private object GetErrorsFromModelState()
+        {
+            return "出错了";
+        }
+    }
+}
---
+++ MvcAjax/Models/PersonValidationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcAjax.Models
+{
+    public class PersonValidationViewModel
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 4)]
+        [Display(Name = "姓")]
+        public string FirstName { set; get; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 4)]
+        [Display(Name = "名")]
+        public string LastName { set; get; }
+
+        [Required]
+        [Display(Name = "姓")]
+        [DataType(DataType.DateTime)]
+        public string BirthDate { set; get; }
+    }
+}
